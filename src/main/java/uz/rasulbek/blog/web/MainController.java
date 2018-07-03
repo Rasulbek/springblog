f2cf68ac76afa18ac11380baeb86ac74fffd1a86
Rasulbek <mr.abdurasuloff@gmail.com>
2018-07-03 16:37:22 +0500

    username on index page

--- src/main/java/uz/rasulbek/blog/web/MainController.java
+++ src/main/java/uz/rasulbek/blog/web/MainController.java
@@ -1,6 +1,8 @@
 package uz.rasulbek.blog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +10,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import uz.rasulbek.blog.BlogModel;
 import uz.rasulbek.blog.BlogRepo;
+import uz.rasulbek.blog.User;
 
 @Controller
 public class MainController {
     @RequestMapping("/")
-    public String getMainPage(){return "index";}
+    public String getMainPage(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        try{
+            User user = (User)  auth.getPrincipal();
+            model.addAttribute("user",user.getUsername());
+        }catch (Exception e){
+            model.addAttribute("user","mehmon");
+        }
+        return "index";
+    }
 
     @RequestMapping("/login")
     public String getLogin(@RequestParam(value = "error", required = false) String error,
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -5,13 +5,23 @@
     <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
 </head>
 <body>
-    <div class="container">
-        <div class="jumbotron">
-            <h1>Blog Post</h1>
-            <p>Bu sayt Spring Boot + Security + Spring JPA + PostgreSQL'lardan foydalanib yaratildi.</p>
+<div class="container">
+    <br>
+    <br>
+    <div class="row">
+        <div class="container">
+            <div class="jumbotron">
+                <div class="h4 text-right">
+                    Hush kelibsiz, ${user}!
+                </div>
+                <h1>Blog Post</h1>
+                <p>Bu sayt Spring Boot + Security + Spring JPA + PostgreSQL'lardan foydalanib yaratildi.</p>
+            </div>
+            <p>Bu yerdan blog postlarga o'tsangiz bo'ladi:</p>
+            <a href="/blog" class="btn btn-success">Bloglar</a>
         </div>
-        <p>Bu yerdan blog postlarga o'tsangiz bo'ladi:</p>
-        <a href="/blog" class="btn btn-success">Bloglar</a>
     </div>
+</div>
+
 </body>
 </html>
